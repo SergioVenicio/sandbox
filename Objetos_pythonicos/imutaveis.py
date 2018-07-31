497b6ce01256fe28c1c8364d86cac323165d22a2
Sérgio <sergio.andrade@autovist.com.br>
2018-07-31 17:39:06 -0300

    Itevaveis

--- Objetos_pythonicos/imutaveis.py
+++ Objetos_pythonicos/imutaveis.py
@@ -35,3 +35,10 @@ s = 'abracadabra'
 
 # Dict Comprehension
 {c: ord(c) for c in s}
+
+
+# Iterável: Pode ser iterado
+# Range retorna um iteravel
+numeros = list(range(10))
+[n * 2 for n in numeros]
+list(map(lambda n: n * 2, numeros))
